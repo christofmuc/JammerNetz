26df38bd1900e373c2a22f5456da015da139d1e1
cruc <christof.ruch@gmx.de>
2019-11-12 11:30:43 +0100

    Doh! I don't need the visual studio developer shell at all when I have CMake.

--- buildWindows.bat
+++ buildWindows.bat
@@ -1,14 +1,8 @@
-@if "%VSCMD_VER%" EQU "" (
-@echo This command file is meant to be run from within a Developer Command Prompt for VS 2017. 
-@echo Please start a VS shell and try again from there!
-exit -1
-)
-
 rem Two step build - first flatbuffers, then the JammerNetz
-cd thirdparty\flatbuffers
+cd third_party\flatbuffers
 cmake -S . -B Builds -G "Visual Studio 15 2017 Win64"
-msbuild Builds\FlatBuffers.sln    
+cmake --build Builds
 cd ..\..
-cmake -S . -B Builds -G "Visual Studio 15 2017 Win64"
-msbuild builds\JammerNetz.sln /p:Configuration=Debug
-msbuild builds\JammerNetz.sln /p:Configuration=Release
+cmake -S . -B Builds\Windows -G "Visual Studio 15 2017 Win64"
+cmake --build Builds\Windows  --config Debug
+cmake --build Builds\Windows  --config Release
