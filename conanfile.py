55bcb6c85bbf2965008bfd01c1011276ccdf8c46
christofmuc <christof.ruch@gmx.de>
2021-10-04 20:13:22 +0200

    Test building with keychain from submodule and not via Conan, as there is no way to build it via Conan for our Ubuntu distribtuion.

--- conanfile.py
+++ conanfile.py
@@ -3,7 +3,7 @@ from conans import ConanFile, CMake
 
 class JammerNetzConan(ConanFile):
     settings = ["os", "compiler", "build_type", "arch"]
-    requires = ["nlohmann_json/3.9.1", "cpprestsdk/2.10.18", "keychain/1.2.0"]
+    requires = ["nlohmann_json/3.9.1", "cpprestsdk/2.10.18"]
     generators = "cmake", "cmake_find_package"
 
     def configure(self):
