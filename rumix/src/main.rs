ec3e037e0573d9e2f712ee73a3af49ff5af93c39
Christof Ruch <christof.ruch@gmx.de>
2022-11-30 13:10:18 +0100

    Build the accept part of the rumix server using a newer version of flatbuffers for better Rust support. This now receives and decodes JammerNetz packages.

--- rumix/src/main.rs
+++ rumix/src/main.rs
@@ -1,3 +1,13 @@
+extern crate flatbuffers;
+
+#[allow(dead_code, unused_imports)]
+#[path = "JammerNetzAudioData_generated.rs"]
+#[path = "JammerNetzPackages_generated.rs"]
+mod JammerNetzAudioData_generated;
+mod JammerNetzPackages_generated;
+pub use JammerNetzAudioData_generated::{JammerNetzPNPAudioData, root_as_jammer_netz_pnpaudio_data };
+pub use JammerNetzPackages_generated::JammerNetzPNPClientInfo;
+
 use tokio::net::UdpSocket;
 use std::io;
 
@@ -8,8 +18,28 @@ async fn main() -> io::Result<()> {
     loop {
         let (len, addr) = sock.recv_from(&mut buf).await?;
         println!("{:?} bytes received from {:?}", len, addr);
+        // The first three bytes are a magic header and the message type
+        if buf[0] as char == '1' && buf[1] as char == '2' && buf[2] as char == '3'
+        {
+            match buf[3]
+            {
+                1u8 => {
+                    // This is an Audio packet, need to use Flatbuffers to deserialize it
+                    let audio_packet = root_as_jammer_netz_pnpaudio_data(&buf[4..]);
+                    if audio_packet.is_ok()
+                    {
+                        let audio_data = audio_packet.unwrap();
+                    }
+                    else {
+                        print("Ignoring corrupted package that can not be decoded by Flatbuffers");
+                    }
+                }
+                _ => println!("Ignoring unknown packet type with identfier {}", buf[3])
+            }
 
-        let len = sock.send_to(&buf[..len], addr).await?;
-        println!("{:?} bytes sent", len);
+        }
+
+       // let len = sock.send_to(&buf[..len], addr).await?;
+       // println!("{:?} bytes sent", len);
     }
 }
